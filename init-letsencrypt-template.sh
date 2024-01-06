c1c60e8448d6b813698bab10e386f02b61fa9a70
Patrick Meaney <patrick.wm.meaney@gmail.com>
2024-01-05 20:21:55 -0600

    updated removed some things and commented out stuff from shell file

--- init-letsencrypt-template.sh
+++ init-letsencrypt-template.sh
@@ -9,7 +9,7 @@ domains=("${NGINX_HOST}" "www.${NGINX_HOST}")
 rsa_key_size=4096
 data_path="./data/certbot"
 email="${ADMIN_EMAIL}" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+# staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -26,49 +26,49 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbotContainerService
-echo
+# echo "### Creating dummy certificate for $domains ..."
+# path="/etc/letsencrypt/live/$domains"
+# mkdir -p "$data_path/conf/live/$domains"
+# docker-compose run --rm --entrypoint "\
+#   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+#     -keyout '$path/privkey.pem' \
+#     -out '$path/fullchain.pem' \
+#     -subj '/CN=localhost'" certbotContainerService
+# echo
 
 
-echo "### Starting nginx ..."
-docker restart nginxContainerService
+# echo "### Starting nginx ..."
+# docker restart nginxContainerService
 # Problem: this wipes the whole nginx container-- do dont use this.
 # docker-compose up --force-recreate -d nginxContainerService
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbotContainerService
-echo
+# echo "### Deleting dummy certificate for $domains ..."
+# docker-compose run --rm --entrypoint "\
+#   rm -Rf /etc/letsencrypt/live/$domains && \
+#   rm -Rf /etc/letsencrypt/archive/$domains && \
+#   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbotContainerService
+# echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
+# echo "### Requesting Let's Encrypt certificate for $domains ..."
 # Join $domains to -d args
-domain_args=()
-for domain in "${domains[@]}"; do
-  domain_args+=("-d" "$domain")
-done
+# domain_args=()
+# for domain in "${domains[@]}"; do
+#   domain_args+=("-d" "$domain")
+# done
 
 # Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
+# case "$email" in
+#   "") email_arg="--register-unsafely-without-email" ;;
+#   *) email_arg="--email $email" ;;
+# esac
 
 # Enable staging mode if needed
-staging_arg=""
-if [ "$staging" -ne 0 ]; then
-  staging_arg="--staging"
-fi
+# staging_arg=""
+# if [ "$staging" -ne 0 ]; then
+#   staging_arg="--staging"
+# fi
 
 # docker-compose run --rm --entrypoint "\
 #   certbot -vvv certonly --webroot -w /var/www/certbot \
